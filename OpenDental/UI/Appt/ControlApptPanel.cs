ff414aff40df581cc0891069e1c78cecbaadca62
Administrator <admin@example.com>
2024-10-29 14:10:28 -0700

    version 24.1.84.0

--- OpenDental/UI/Appt/ControlApptPanel.cs
+++ OpenDental/UI/Appt/ControlApptPanel.cs
@@ -3659,6 +3659,9 @@ namespace OpenDental.UI{
 					case EnumApptViewElement.PatientNamePref:
 						text=dataRoww["PatientNamePref"].ToString();
 						break;
+					case EnumApptViewElement.PatientWard:
+						text="Patient Ward: "+dataRoww["patientWard"].ToString();
+						break;
 					case EnumApptViewElement.PatNum:
 						text=dataRoww["patNum"].ToString();
 						break;
